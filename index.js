d68350157b97f57af03874ef8b7df29202071eeb
urceyhan-ctrl <urceyhan@gmail.com>
2026-08-15 10:40:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -506,3 +506,10 @@ process.once('SIGINT', () => shutdown('SIGINT'));
 process.once('SIGTERM', () => shutdown('SIGTERM'));
 
 client.login(token);
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is running!');
+});
+server.listen(process.env.PORT || 3000);
